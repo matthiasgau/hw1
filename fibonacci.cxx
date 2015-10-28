73bce9dcf591b342204233c211685d5099441aba
Matthias <matthias@linux-xoxy.site>
2015-10-28 19:15:20 +0100

    did my homework

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int i=0;
+  int j=1;
+  int sum;
+  int N; 
+  cout << "N" << endl;
+  cin >> N; 
+  for(int k=1; k<N; k++){
+    sum=i+j;
+    i=j;
+    j=sum;
+  }
+  cout << j << endl;
+ return 0;
+}
+    
